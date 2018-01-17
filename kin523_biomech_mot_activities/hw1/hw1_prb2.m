cf05fee7a78647068b7c61a874e74f7249016a30
leanth <leanth@oregonstate.edu>
2018-01-16 18:29:31 -0800

    Redo hw1 problems for KIN 523 and edit respective code

--- kin523_biomech_mot_activities/hw1/hw1_prb2.m
+++ kin523_biomech_mot_activities/hw1/hw1_prb2.m
@@ -15,11 +15,9 @@ MH3 = [MH3X, MH3Y, MH3Z];
 MH5 = [MH5X, MH5Y, MH5Z];
 WRI = [WRIX, WRIY, WRIZ];
 
-% wrist joint center offset from WRI marker
-offset = [-11, 0, -21];
-
-% actual wrist joint center position in global XYZ
-WJC = WRI + offset;
+% wrist joint center position in local hand xyz
+% from problem 1c
+WJC_xyz = [-8.13912848115047; 20.8543822348700; -7.80059800074728];
 
 % u vector from WRI to MH3 markers
 U = MH3 - WRI; % x-axis
@@ -55,51 +53,30 @@ UNIT_VECS_stacked = cat(3, I, J, K);
 UNIT_VECS_rs = permute(UNIT_VECS_stacked, [2, 3, 1]);
 UNIT_VECS_rs2 = permute(UNIT_VECS_stacked, [3, 2, 1]);
 
-% [rows, cols] = size(Time);
-
 % global XYZ origin
 XYZ = [0, 0, 0];
 X = [1, 0, 0];
 Y = [0, 1, 0];
 Z = [0, 0, 1];
 
-% create empty arrays
-WJC_xyz = [];
-WJC_XYZ = [];
-WJC_cov1 = [];
-WJC_cov2 = [];
-
-% conversion of wrist joint center position in local hand xyz
-for rows = 1:size(UNIT_VECS_rs2, 3)
-    WJC_cov1 = UNIT_VECS_rs2(:, :, rows) * transpose(WJC(rows, :) - WRI(rows, :));
-    WJC_xyz = cat(2, WJC_xyz, WJC_cov1);
-end
-WJC_xyz = transpose(WJC_xyz);
-
 % conversion of wrist joint center position in global XYZ
-for rows = 1:size(UNIT_VECS_rs, 3)
-    WJC_cov2 = (UNIT_VECS_rs(:, :, rows) * transpose(WJC_xyz(rows, :))) + transpose(WRI(rows, :));
-    WJC_XYZ = cat(2, WJC_XYZ, WJC_cov2);
-end
+WJC_XYZ = (transpose([I(1, :); J(1, :); K(1, :)]) * WJC_xyz) + transpose(WRI(1, :));
 WJC_XYZ = transpose(WJC_XYZ);
 
 % table for problem 2c
-table_2c = [Time, WJC_XYZ];
+table_2c = [Time(1,:) , WJC_XYZ];
 
 % unit vector for hand's anterior axis in global XYZ
 e_XYZ = [0; 0; -1];
 
+e_ijk = [0.129213880302701; 0.910385524181768; -0.393067386713025];
+
 % create empty array
 E = [];
 
-% values of e_x, e_y, e_z corresponding to the components of e_XYZ in the
-% I, J, K directions
-% e_IJK = UNIT_VECS_rs .\ e_XYZ;
-% for rows = 1:size(e_IJK, 3)
-%     E = cat(1, E, e_IJK(3, :, rows));
-% end
+% global XYZ components of e in the global XYZ coordinate system
 for rows = 1:size(UNIT_VECS_rs, 3)
-    e_IJK = UNIT_VECS_rs(:, :, rows) \ e_XYZ;
+    e_IJK = UNIT_VECS_rs(:, :, rows) * e_ijk;
     E = cat(2, E, e_IJK);
 end
 E = transpose(E);
