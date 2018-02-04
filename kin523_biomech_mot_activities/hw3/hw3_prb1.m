45c20afafdbfeee461574356277ef7d0385c267b
leanth <leanth@oregonstate.edu>
2018-02-04 00:45:49 -0800

    Start hw3 for KIN 523

---
+++ kin523_biomech_mot_activities/hw3/hw3_prb1.m
@@ -0,0 +1,41 @@
+% Created by: Anthony Le
+% Last updated: 02.04.2018
+
+% KIN 523: Homework 3 - Signal Processing
+% Due: 02.06.2018
+%% Problem 1
+close all;
+clear;
+
+% part a
+f_c = 5; % in Hz; cut-off freq
+f_s = 60; % in frames/s; sampling freq
+T = 1 / f_s; % in s per frame; sampling period
+
+Omega_C = tan(pi * (f_c / f_s)) / 0.802;
+
+a_0 = (Omega_C^2) / (1 + sqrt(2) * Omega_C + Omega_C^2);
+a_1 = 2 * a_0;
+a_2 = a_0;
+
+b_1 = (2 * (1 - Omega_C^2)) / (1 + sqrt(2) * Omega_C + Omega_C^2);
+b_2 = 1 - (a_0 + a_1 + a_2 + b_1);
+
+% part b
+k = linspace(24, 28, 5);
+X_kT = [559.65; 569.50; 589.97; 610.15; 626.80];
+X_kT_back = flipud(X_kT);
+Y1_kT = [538.17; 547.19];
+Y2_kT = [629.36; 608.56];
+
+for i = 3:5
+    y1_kT = a_0 * X_kT(i) + a_1 * X_kT(i-1) + a_2 * X_kT(i-2) + b_1 * Y1_kT(i-1) + b_2 * Y1_kT(i-2);
+    Y1_kT = cat(1, Y1_kT, y1_kT);
+end
+
+Y1_kT_back = flipud(Y1_kT);
+
+for j = 3:5
+    y2_kT = a_0 * Y1_kT_back(j) + a_1 * Y1_kT_back(j-1) + a_2 * Y1_kT_back(j-2) + b_1 * Y2_kT(j-1) + b_2 * Y2_kT(j-2);
+    Y2_kT = cat(1, Y2_kT, y2_kT);
+end
---
+++ kin523_biomech_mot_activities/hw3/hw3_prb2.m
@@ -0,0 +1,12 @@
+% Created by: Anthony Le
+% Last updated: 02.04.2018
+
+% KIN 523: Homework 3 - Signal Processing
+% Due: 02.06.2018
+%% Problem 2
+close all;
+clear;
+
+time = [0.7000; 0.7167; 0.7333; 0.7500; 0.7667];
+knee_ang = [30.2; 31.3; 33.9; 37.8; 43.2];
+
