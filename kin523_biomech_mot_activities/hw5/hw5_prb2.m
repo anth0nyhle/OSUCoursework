beecbf825d43594d8889faf0671c19d0e355111f
leanth <leanth@oregonstate.edu>
2018-02-21 11:09:36 -0800

    Finish prb1 of hw5 for KIN 523

--- kin523_biomech_mot_activities/hw5/hw5_prb2.m
+++ kin523_biomech_mot_activities/hw5/hw5_prb2.m
@@ -1,5 +1,5 @@
 % Created by: Anthony Le
-% Last updated: 02.20.2018
+% Last updated: 02.21.2018
 
 % KIN 523: Homework 5 - 3D Kinematics & Anthropometry
 % Due: 02.22.18
@@ -7,10 +7,15 @@
 close all;
 clear;
 
-i_T
-j_T
-k_T
+% orientation of anatomical xyz unit vectors of thigh in global XYZ at
+% instant of ground contact
+i_T = [0.962; 0.110; -0.251];
+j_T = [-0.258; 0.053; -0.965];
+k_T = [-0.093; 0.992; 0.080];
 
-i_L
-j_L
-k_L
+% orientation of anatomical xyz unit vectors of leg in global XYZ at
+% instant of ground contact
+i_L = [0.991; -0.103; 0.085];
+j_L = [0.091; 0.050; -0.995];
+k_L = [0.098; 0.993; 0.059];
+
