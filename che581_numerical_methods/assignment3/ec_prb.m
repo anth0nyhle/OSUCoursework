360d48feb736f926722077ab72b9b13116361de6
leanth <leanth@oregonstate.edu>
2019-02-12 09:33:22 -0800

    Finish ec problem for assignment 3 for CHE 581

--- che581_numerical_methods/assignment3/ec_prb.m
+++ che581_numerical_methods/assignment3/ec_prb.m
@@ -11,29 +11,33 @@ clc; % clear command window
 
 disp('Extra Credit')
 
-n = 20;
+n = 4;
 % sig_x = 1;
 % sig_y = 1;
 f_xy = @(x, y) (1 / (2 * pi * 1 * 1)) * exp(-(x^2 / (2 * 1^2)) - (y^2 / (2 * 1^2)));
-% f_xy = @(x, y) (1 / (2 * pi * 1 * 1)) * exp(-(x.^2 / (2 * 1^2)) - (y.^2 / (2 * 1^2)));
+f_xy1 = @(x, y) (1 / (2 * pi * 1 * 1)) * exp(-(x.^2 / (2 * 1^2)) - (y.^2 / (2 * 1^2)));
 
 ax_1 = -1;
 bx_1 = 1;
 ay_1 = -1;
 by_1 = 1;
 I_1 = twoDtrap(f_xy, ax_1, bx_1, ay_1, by_1, n);
-% I_1 = integral2(f_xy, ax_1, bx_1, ay_1, by_1);
+% I = integral2(f_xy1, ax_1, bx_1, ay_1, by_1);
+fprintf('I_1 = %5.4f\n', I_1);
 
 ax_2 = -2;
 bx_2 = 2;
 ay_2 = -2;
 by_2 = 2;
-% I_2 = twoDtrap(f_xy, ax_2, bx_2, ay_2, by_2, n, sig_x, sig_y);
+I_2 = twoDtrap(f_xy, ax_2, bx_2, ay_2, by_2, n);
+I = integral2(f_xy1, ax_2, bx_2, ay_2, by_2);
+fprintf('I_2 = %5.4f\n', I_2);
 
 ax_3 = -3;
 bx_3 = 3;
 ay_3 = -3;
 by_3 = 3;
-% I_3 = twoDtrap(f_xy, ax_3, bx_3, ay_3, by_3, n, sig_x, sig_y);
+I_3 = twoDtrap(f_xy, ax_3, bx_3, ay_3, by_3, n);
+fprintf('I_3 = %5.4f\n', I_3);
 
 disp('-------------------------------------------------');
--- che581_numerical_methods/assignment3/twoDtrap.m
+++ che581_numerical_methods/assignment3/twoDtrap.m
@@ -4,25 +4,50 @@ x_a = ax; % initialize 1st x point
 y_a = ay; % initialize 1st y point
 wx = (bx - ax) / n; % base width, dependent on n number of segments
 wy = (by - ay) / n; % base width, dependent on n number of segments
-S = zeros(n, 2); % allocate memory based on n number of segments
+SX = []; % allocate memory based on n number of segments
+SXY = [];
+
+% modified trap code for 2-dim integration
+% for i = 1:n-2
+%     y_b = y_a + (wy / 2); % next point
+%     y_c = y_b + (wy / 2);
+%     for j = 1:n
+%         x_b = x_a + (wx / 2); % next point
+%         x_c = x_b + (wx / 2);
+%         avg_hx = (func(x_a, y_a, varargin{:}) + (2 * func(x_b, y_b, varargin{:})) + func(x_c, y_c, varargin{:})) / 4; % avg height
+%         sx = wx * avg_hx;
+%         SX(j, 1) = sx; % store computed area of segment
+%         x_a = x_c; % next segment
+%     end
+%     avg_hy = (SX(i) + (2 * SX(i+1)) + SX(i+2)) / 4;
+%     sxy = wy * avg_hy;
+%     SXY(i, 1) = sxy;
+%     y_a = y_c; % next segment
+% end
+% 
+% I = sum(SXY); % sum of all segment areas
 
 % modified trap code for 2-dim integration
 for i = 1:n
-    y_b = y_a + (wy / 2); % next point
-    y_c = y_b + (wy / 2);
     for j = 1:n
-        x_b = x_a + (wx / 2); % next point
-        x_c = x_b + (wx / 2);
-        avg_hx = (func(x_a, y_a, varargin{:}) + (2 * func(x_b, y_b, varargin{:})) + func(x_c, y_c, varargin{:})) / 4; % avg height
+        x_b = x_a + wx; % next point
+        avg_hx = (func(x_a, y_a, varargin{:}) + func(x_b, y_a, varargin{:})) / 2; % avg height
         sx = wx * avg_hx;
-        SX(i, 1) = sx; % store computed area of segment
-        x_a = x_c; % next segment
+        SX(j, i) = sx; % store computed area of segment
+        x_a = x_b; % next segment
     end
-    avg_hy = (SX(i) + (2 * SX(i+1)) + SX(i+2)) / 4;
-    sy = wy * avg_hy;
-%     y_a = y_c; % next segment
+    y_b = y_a + wy; % next point
+    y_a = y_b; % next segment
 end
 
-I = sum(S); % sum of all segment areas
+SX = sum(SX, 1);
+
+for k = 1:n-1
+    avg_hy = (SX(k) + SX(k+1)) / 2;
+    sxy = wy * avg_hy;
+    SXY(k, 1) = sxy;
+end
+
+I = sum(SXY); % sum of all segment areas
 
 end
