d5d98789942f9c784c5d27296780e1292d2bbaa5
leanth <leanth@oregonstate.edu>
2019-02-19 11:35:59 -0800

    Create script files for assignment 5 CHE 581

---
+++ che581_numerical_methods/assignment5/ch22prbs.m
@@ -0,0 +1,71 @@
+% Created by: Anthony H. Le
+% Last updated: 02-19-2019
+
+% CHE 581: Assignment 5
+% Textbook Problems 22.1, 22.6, 22.10, 22.11, 22.12. 22.14
+% Due: 02-27-2019
+%% Problem 22.1
+close all; % close all figures
+clear; % clear workspace
+clc; % clear command window
+
+disp('Problem 22.1');
+
+
+
+disp('-------------------------------------------------');
+
+%% Problem 22.6
+close all;
+clear;
+clc;
+
+disp('Problem 22.6');
+
+
+
+disp('-------------------------------------------------');
+
+%% Problem 22.10
+close all;
+clear;
+clc;
+
+disp('Problem 22.10');
+
+
+
+disp('-------------------------------------------------');
+
+%% Problem 22.11
+close all;
+clear;
+clc;
+
+disp('Problem 22.11');
+
+
+
+disp('-------------------------------------------------');
+
+%% Problem 22.12
+close all;
+clear;
+clc;
+
+disp('Problem 22.12');
+
+
+
+disp('-------------------------------------------------');
+
+%% Problem 22.14
+close all;
+clear;
+clc;
+
+disp('Problem 22.14');
+
+
+
+disp('-------------------------------------------------');
---
+++ che581_numerical_methods/assignment5/ec_prb.m
@@ -0,0 +1,16 @@
+% Created by: Anthony H. Le
+% Last updated: 02-19-2019
+
+% CHE 581: Assignment 5
+% Extra Credit Problem
+% Due: 02-27-2019
+%% Extra Credit
+close all; % close all figures
+clear; % clear workspace
+clc; % clear command window
+
+disp('Extra Credit Problem');
+
+
+
+disp('-------------------------------------------------');
