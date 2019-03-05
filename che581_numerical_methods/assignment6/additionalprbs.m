3eec88ce4a826b2db0d29ddb987112ef7fe4829d
leanth <leanth@oregonstate.edu>
2019-03-05 12:50:01 -0800

    Start assignment 6 for CHE 581

---
+++ che581_numerical_methods/assignment6/additionalprbs.m
@@ -0,0 +1,27 @@
+% Created by: Anthony H. Le
+% Last updated: 03-05-2019
+
+% CHE 581: Assignment 6
+% Textbook Problems 24.8, 24.11, Additional Problems 1, 2
+% Due: 03-13-2019
+%% Additional Problem 1
+close all; % close all figures
+clear; % clear workspace
+clc; % clear command window
+
+disp('Additional Problem 1');
+
+
+
+disp('-------------------------------------------------');
+
+%% Additional Problem 2
+close all;
+clear;
+clc;
+
+disp('Additional Problem 2');
+
+
+
+disp('-------------------------------------------------');
---
+++ che581_numerical_methods/assignment6/ch24prbs.m
@@ -0,0 +1,31 @@
+% Created by: Anthony H. Le
+% Last updated: 03-05-2019
+
+% CHE 581: Assignment 6
+% Textbook Problems 24.8, 24.11
+% Due: 03-13-2019
+%% Problem 24.8
+close all; % close all figures
+clear; % clear workspace
+clc; % clear command window
+
+disp('Problem 24.8');
+
+% (a) shooting method
+disp('(a) shooting method');
+
+
+% (b) finite-difference method (delta x = 2)
+disp('(b) finite-diff method');
+
+
+disp('-------------------------------------------------');
+
+%% Problem 24.11
+close all;
+clear;
+clc;
+
+disp('Problem 24.11');
+
+disp('-------------------------------------------------');
